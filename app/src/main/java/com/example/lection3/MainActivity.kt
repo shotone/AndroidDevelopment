01e41c37f1338de70a41b62d7c5b16253c0aac64
Shota Firanashvili <shota.firanashvili.1@btu.edu.ge>
2020-03-28 15:05:53 +0400

    Empty

---
+++ app/src/main/java/com/example/lection3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lection3
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log.d
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
